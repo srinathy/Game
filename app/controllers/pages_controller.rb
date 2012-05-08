0bbb20f423636b317dae9f58b67c87eab77a7e62
Plumbly <josh.plumbly.09@aberdeen.ac.uk>
2012-05-08 19:16:00 +0100

    Completed basic layout for siteand generated Product code

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  
+  def home
+    @title = "Welcome to Game's Central!"
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Game::Application.config.secret_token = '7a4a216f151045498384f1257565bb433f82d7ce4adcccc48192d7da97af9201c5db6cf6cfd36a12f44328064a9f5b4d7698e38b496de14178a05f81ee111d2a'
